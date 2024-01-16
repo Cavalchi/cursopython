bbc2a2911e8c9e5c837c3205afb99d65f8858b58
Cavalchi <jpcavalchikar@hotmail.com>
2024-01-16 01:45:32 -0300

    update

--- funcoes.py
+++ funcoes.py
@@ -3,12 +3,19 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 def play_pause_spotify():
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id='seu-client-id',
-                                                   client_secret='seu-client-secret',
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id='f31f836299394c619b9a089f73ada300',
+                                                   client_secret='955ad2963f444c2d9ac40a46f271f690',
                                                    redirect_uri='http://localhost:8888/callback',
-                                                   scope='user-modify-playback-state'))
+                                                   scope='user-modify-playback-state user-read-playback-state'))
     playback_info = sp.current_playback()
-    if playback_info['is_playing']:
+    if playback_info is not None and playback_info['is_playing']:
         sp.pause_playback()
     else:
         sp.start_playback()
+
+def next_track_spotify():
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id='f31f836299394c619b9a089f73ada300',
+                                                   client_secret='955ad2963f444c2d9ac40a46f271f690',
+                                                   redirect_uri='http://localhost:8888/callback',
+                                                   scope='user-modify-playback-state user-read-playback-state'))
+    sp.next_track()
--- maos.py
+++ maos.py
@@ -1,18 +1,18 @@
 import cv2
 import mediapipe as mp
 import numpy as np
-from funcoes import play_pause_spotify  # Adicione esta linha
+from funcoes import play_pause_spotify, next_track_spotify
 
 # Inicializa o mediapipe
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 hands = mp_hands.Hands()
 
-# Adicione esta linha
 polegar_indicador_levantado_anteriormente = False
+polegar_medio_indicador_levantado_anteriormente = False
 
 def reconhecer_maos(frame):
-    global polegar_indicador_levantado_anteriormente  # Adicione esta linha
+    global polegar_indicador_levantado_anteriormente, polegar_medio_indicador_levantado_anteriormente
 
     # Converte a imagem para RGB
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -29,22 +29,26 @@ def reconhecer_maos(frame):
             dedos = ['Polegar', 'Indicador', 'Medio', 'Anelar', 'Minimo']
             dedos_levantados = []
             for i, dedo in enumerate(dedos):
-                # O ponto de referência 0 é o pulso.
-                # Para cada dedo, o ponto de referência 4*i+1 é a base do dedo.
-                # O ponto de referência 4*i+4 é a ponta do dedo.
                 base = np.array([hand_landmarks.landmark[4*i+1].x, hand_landmarks.landmark[4*i+1].y])
                 ponta = np.array([hand_landmarks.landmark[4*i+4].x, hand_landmarks.landmark[4*i+4].y])
                 distancia = np.linalg.norm(base - ponta)
-                if distancia > 0.1:  # Ajuste este valor conforme necessário
+                if distancia > 0.1:
                     dedos_levantados.append(dedo)
 
             # Verifica gestos específicos
             if set(['Polegar', 'Indicador']).issubset(dedos_levantados) and len(dedos_levantados) == 2:
                 if not polegar_indicador_levantado_anteriormente:
-                    play_pause_spotify()  # Adicione esta linha
+                    play_pause_spotify()
                 polegar_indicador_levantado_anteriormente = True
             else:
                 polegar_indicador_levantado_anteriormente = False
+
+            if set(['Polegar', 'Indicador', 'Medio']).issubset(dedos_levantados) and len(dedos_levantados) == 3:
+                if not polegar_medio_indicador_levantado_anteriormente:
+                    next_track_spotify()
+                polegar_medio_indicador_levantado_anteriormente = True
+            else:
+                polegar_medio_indicador_levantado_anteriormente = False
 
             if set(['Polegar', 'Indicador']).issubset(dedos_levantados) and len(dedos_levantados) == 2:
                 cv2.putText(frame, 'Arminha', (int(hand_landmarks.landmark[0].x * frame.shape[1]), int(hand_landmarks.landmark[0].y * frame.shape[0]) - 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
@@ -52,7 +56,6 @@ def reconhecer_maos(frame):
                 cv2.putText(frame, 'PAZ', (int(hand_landmarks.landmark[0].x * frame.shape[1]), int(hand_landmarks.landmark[0].y * frame.shape[0]) - 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
             else:
                 for dedo in dedos_levantados:
-                    # Para o polegar, verifica se está perto do indicador
                     if dedo == 'Polegar' and 'Indicador' in dedos_levantados:
                         continue
                     cv2.putText(frame, dedo, (int(hand_landmarks.landmark[4*dedos.index(dedo)+4].x * frame.shape[1]), int(hand_landmarks.landmark[4*dedos.index(dedo)+4].y * frame.shape[0]) - 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
